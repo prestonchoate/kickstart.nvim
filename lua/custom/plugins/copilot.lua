fd591cc131564aea860f3a63de772274b36c24b5
Preston Choate <pchoate@adobe.com>
2023-12-15 15:53:32 -0500

    Fixed config for blankline package, added config for Copilot

---
+++ lua/custom/plugins/copilot.lua
@@ -0,0 +1,52 @@
+return {
+
+  -- copilot
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    opts = {
+      suggestion = { enabled = true },
+      panel = { enabled = false },
+      filetypes = {
+        markdown = true,
+        help = true,
+      },
+    },
+  },
+  -- copilot cmp source
+  {
+    "nvim-cmp",
+    dependencies = {
+      {
+        "zbirenbaum/copilot-cmp",
+        dependencies = "copilot.lua",
+        opts = {},
+        config = function(_, opts)
+          local copilot_cmp = require("copilot_cmp")
+          copilot_cmp.setup(opts)
+          -- attach cmp source whenever copilot attaches
+          -- fixes lazy-loading issues with the copilot cmp source
+          vim.api.nvim_create_autocmd("LspAttach", {
+            callback = function(args)
+              local buffer = args.buf
+              local client = vim.lsp.get_client_by_id(args.data.client_id)
+              if client.name == "copilot" then
+                copilot_cmp._on_insert_enter({ bufnr = buffer })
+              end
+            end,
+          })
+        end,
+      },
+    },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      opts.sources = opts.sources or {}
+      table.insert(opts.sources, 1, {
+        name = "copilot",
+        group_index = 1,
+        priority = 100,
+      })
+    end,
+  }
+}
